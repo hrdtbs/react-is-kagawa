c544c984f56d8e0b39f8f33cb5addeb507b72345
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-03-22 17:19:22 +0900

    feat: add useConfirmIsKagawa hooks

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
-# blocking japan
+# React is Kagawa?
 
-Just loading this script makes it difficult to access the site from Japan.
+```tsx
+const {isKagawa, open} = useConfirmIsKagawa()
 
-```html
-<script src="https://cdn.jsdelivr.net/gh/hrdtbs/blocking-japan@1.0.0/dist/index.cjs.js"></script>
+useEffect(() => {
+    open() // 本サービスはゲーム要素を含んでいます。あなたは香川県民ですか？
+},[])
+```
+
+```tsx
+const {isKagawa, open} = useGeographicallyConfirmIsKagawa()
+
+useEffect(() => {
+    /**
+     * Geolocation APIを許可しなかった場合、
+     * または緯度経度が香川近辺である場合のみプロンプトが開きます。
+     */
+    open() 
+},[])
+```
+
+```tsx
+const {isKagawa, open} = useStrictConfirmIsKagawa()
+
+useEffect(() => {
+    /**
+     * 「本サービスはゲーム要素を含んでいます。あなたは香川県民ですか？」
+     * Yes →「18歳未満ですか？」
+     *    Yes →「学習目的ですか？」
+     *        No → isKagawaがtrueに
+     *        Yes → 「本日は既にゲームをしていますか？
+     *               また本日このサービス以外にゲームを行わないことを了承しますか？」
+     *          Yes → 一時間後にisKagawaがtrueに
+     */
+    open() 
+},[])
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,80 @@
-;(() => {
-    const kill = () => {
-        alert("日本ではこのページを見ることができません")
-        document.body.innerHTML = ""
-        document.head.innerHTML = ""
+import { useCallback, useState } from "react"
+
+const CONFIRM_IS_KAGAWA_MESSAGE = "本サービスはゲーム要素を含んでいます。あなたは香川県民ですか？"
+
+export const useConfirmIsKagawa = () => {
+    const [isKagawa, setIsKagawa] = useState(false)
+
+    const open = useCallback(() => {
+        const answer = window.confirm(CONFIRM_IS_KAGAWA_MESSAGE)
+        setIsKagawa(answer)
+    }, [])
+
+    return {
+        isKagawa,
+        open
     }
-    if (navigator.language !== "ja") return
-    navigator.geolocation.getCurrentPosition(
-        position => {
-            const lat = position.coords.latitude
-            const lng = position.coords.longitude
-            if ((30.591 <= lat && lat <= 45.3122) || (129.339 <= lng && lng <= 145.49)) {
-                kill()
+}
+
+export const useGeographicallyConfirmIsKagawa = () => {
+    const [isKagawa, setIsKagawa] = useState(false)
+
+    const ask = useCallback(() => {
+        const answer = window.confirm(CONFIRM_IS_KAGAWA_MESSAGE)
+        setIsKagawa(answer)
+    }, [])
+
+    const open = useCallback(() => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const lat = position.coords.latitude
+                const lng = position.coords.longitude
+                if ((33 <= lat && lat <= 35) || (133 <= lng && lng <= 135)) {
+                    ask()
+                }
+            },
+            () => {
+                ask()
             }
-        },
-        () => {
-            kill()
+        )
+    }, [ask])
+
+    return {
+        isKagawa,
+        open
+    }
+}
+
+export const useStrictConfirmIsKagawa = () => {
+    const [isKagawa, setIsKagawa] = useState(false)
+
+    const open = useCallback(() => {
+        const answer = window.confirm(CONFIRM_IS_KAGAWA_MESSAGE)
+        if (answer) {
+            const isUnder18 = window.confirm("18歳未満ですか？")
+            if (isUnder18) {
+                const isForLearning = window.confirm("学習目的ですか？")
+                if (!isForLearning) {
+                    setIsKagawa(true)
+                } else {
+                    const isAlreadyPlayGameToday = window.confirm(
+                        "本日は既にゲームをしていますか？また本日このサービス以外にゲームを行わないことを了承しますか？"
+                    )
+                    if (isAlreadyPlayGameToday) {
+                        setIsKagawa(true)
+                    } else {
+                        window.confirm("一時間のみ本サービスを利用することができます。")
+                        window.setTimeout(() => {
+                            setIsKagawa(true)
+                        }, 60 * 60 * 1000)
+                    }
+                }
+            }
         }
-    )
-})()
+    }, [])
+
+    return {
+        isKagawa,
+        open
+    }
+}
